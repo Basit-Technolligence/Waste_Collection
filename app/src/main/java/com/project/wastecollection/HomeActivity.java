6b07512a696046e7c56d3caff7e257b5b63e674c
basit96 <pakkobra007@gmail.com>
2019-12-11 11:06:03 +0500

    Correct home page

--- app/src/main/java/com/project/wastecollection/HomeActivity.java
+++ app/src/main/java/com/project/wastecollection/HomeActivity.java
@@ -8,111 +8,139 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
-public class HomeActivity extends AppCompatActivity implements OnMapReadyCallback,NavigationView.OnNavigationItemSelectedListener{
+public class HomeActivity extends AppCompatActivity implements OnMapReadyCallback, NavigationView.OnNavigationItemSelectedListener {
     private static final int REQUEST_LOCATION = 0;
     LocationManager locationManager;
-    private String provider;
+    LocationListener locationListener;
     DrawerLayout drawerLayout;
     ImageView imageView;
     ActionBarDrawerToggle drawerToggle;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        drawerLayout= (DrawerLayout) findViewById(R.id.drawarLayout);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawarLayout);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         //adding drawar button
-        drawerToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(drawerToggle);
         drawerToggle.syncState();
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //navbar item click
-        NavigationView navigationView=(NavigationView) findViewById(R.id.navigationView);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigationView);
         navigationView.setNavigationItemSelectedListener(this);
 
         //header click navbar
         View headerview = navigationView.getHeaderView(0);
-        imageView=(ImageView) headerview.findViewById(R.id.profile_image);
+        imageView = (ImageView) headerview.findViewById(R.id.profile_image);
 
         //opening map fragment
-        SupportMapFragment supportMapFragment =(SupportMapFragment)
+        SupportMapFragment supportMapFragment = (SupportMapFragment)
                 getSupportFragmentManager().findFragmentById(R.id.fragmentMap);
         supportMapFragment.getMapAsync(HomeActivity.this);
 
-
-
-
-
-
     }
-
-
-
-
 
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        //GetLocation();
-        Criteria criteria = new Criteria();
-        provider = locationManager.getBestProvider(criteria, false);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,new String[]
-                    {Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_LOCATION);
-        }
-        Location location = locationManager.getLastKnownLocation(provider);
-        if (location!=null){
-            double lat= location.getLatitude();
-            double lon=location.getLongitude();
+    public void onMapReady(final GoogleMap googleMap) {
 
-            //showing on map
-            LatLng latLng = new LatLng(lat, lon);
-            Toast.makeText(this, " "+lat+" "+lon, Toast.LENGTH_LONG).show();
-            googleMap.addMarker(new MarkerOptions().position(latLng).title("Your location"));
-            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,18),4000,null);
-        }
-        else{
 
-            Snackbar.make(drawerLayout, "Please allow location to this app", Snackbar.LENGTH_LONG).show();
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                googleMap.clear();
+
+
+                //Toast.makeText(HomeActivity.this, "location:" + location.getLatitude(), Toast.LENGTH_SHORT).show();
+                //showing on map
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                googleMap.addMarker(new MarkerOptions().position(latLng).title("Your location"));//.icon(BitmapDescriptorFactory.fromResource(R.drawable.dp)));
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,18),4000,null);
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+              //  Toast.makeText(HomeActivity.this, "status change", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+             //   Toast.makeText(HomeActivity.this, "Permission", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(HomeActivity.this, new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
         }
+
+        locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+
+
 
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+
+    }
+
 
     //drawer open close click
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(drawerToggle.onOptionsItemSelected(item)){
+        if (drawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private double GetLocation(){
-        //Location locationNetwork=locationManager.getLastKnownLocation(NETW)
-        return 2.2;
-    }
 
 
     @Override
@@ -139,4 +167,5 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
         return false;
     }
+
 }
