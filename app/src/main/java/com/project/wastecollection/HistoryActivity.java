bc5a21872a2fb973d5262dd476dfb036d2984a3d
basit96 <pakkobra007@gmail.com>
2019-12-12 15:58:52 +0500

    add drawar as base activity

--- app/src/main/java/com/project/wastecollection/HistoryActivity.java
+++ app/src/main/java/com/project/wastecollection/HistoryActivity.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.widget.LinearLayout;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,21 +29,23 @@ public class HistoryActivity extends BaseActivity{
     ArrayList<String> latitude=new ArrayList<>();
     ArrayList<String> longitude=new ArrayList<>();
     DatabaseReference database=FirebaseDatabase.getInstance().getReference("History");
-    String currentUser,checkUser;
+    String checkUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_history);
         recyclerView=(RecyclerView) findViewById(R.id.recyclerView);
          recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        currentUser="ali";
+        FirebaseUser currentUser= FirebaseAuth.getInstance().getCurrentUser();
+        final String uid=currentUser.getUid();
         database.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for(DataSnapshot ds_history:dataSnapshot.getChildren())
                 if(ds_history.exists()){
                     checkUser=ds_history.child("user").getValue().toString();
-                    if(checkUser.equals(currentUser)){
+                    if(checkUser.equals(uid)){
                       dustbinName.add(ds_history.child("dustbin").getValue().toString());
                       date.add(ds_history.child("date").getValue().toString());
                       time.add(ds_history.child("time").getValue().toString());
