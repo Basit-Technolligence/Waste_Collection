adf3ddc680042015a8bb69df78a0c5c71e6ab939
Aashir-Technolligence <aashi7651@gmail.com>
2019-12-15 07:16:12 +0500

    issues added

--- app/src/main/java/com/project/wastecollection/IssueActivity.java
+++ app/src/main/java/com/project/wastecollection/IssueActivity.java
@@ -1,13 +1,22 @@
 package com.project.wastecollection;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -16,8 +25,10 @@ import com.google.firebase.database.ValueEventListener;
 
 public class IssueActivity extends BaseActivity {
     Button accident,itemFound,vechicleProblem,tripIssue,appIsuue;
-    String i1,i2,i3,i4,i5;
     DatabaseReference dref= FirebaseDatabase.getInstance().getReference();
+    String latitude, longitude;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,30 +43,116 @@ public class IssueActivity extends BaseActivity {
         vechicleProblem = (Button)findViewById( R.id.issue3 );
         tripIssue = (Button)findViewById( R.id.issue4 );
         appIsuue = (Button)findViewById( R.id.issue5 );
+        FirebaseUser curentUser = FirebaseAuth.getInstance().getCurrentUser();
+        final String uid = curentUser.getUid();
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    latitude = String.valueOf(location.getLatitude());
+                    longitude = String.valueOf(location.getLongitude());
+                }
+            }
+        });
         accident.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final String push = FirebaseDatabase.getInstance().getReference().child("History").push().getKey();
+                dref.child("Issues").child(push).child("id").setValue(push);
+                dref.child("Issues").child(push).child("longitude").setValue(longitude);
+                dref.child("Issues").child(push).child("latitude").setValue(latitude);
+                dref.child("Issues").child(push).child("driverid").setValue(uid);
+                dref.child("Issues").child(push).child("issue").setValue("I got an accident.");
 
-                dref.addValueEventListener( new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getApplicationContext());
+                alertDialogBuilder.setTitle("Issue Submitted").setMessage("We are sending help for you.").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
                     }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                } );
+                }).show();
 
             }
         } );
 
+        vechicleProblem.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String push = FirebaseDatabase.getInstance().getReference().child("History").push().getKey();
+                dref.child("Issues").child(push).child("id").setValue(push);
+                dref.child("Issues").child(push).child("longitude").setValue(longitude);
+                dref.child("Issues").child(push).child("latitude").setValue(latitude);
+                dref.child("Issues").child(push).child("driverid").setValue(uid);
+                dref.child("Issues").child(push).child("issue").setValue("I have a problem with my vehicle.");
 
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getApplicationContext());
+                alertDialogBuilder.setTitle("Issue Submitted").setMessage("We are sending help for you.").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).show();
 
+            }
+        } );
+        itemFound.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String push = FirebaseDatabase.getInstance().getReference().child("History").push().getKey();
+                dref.child("Issues").child(push).child("id").setValue(push);
+                dref.child("Issues").child(push).child("longitude").setValue(longitude);
+                dref.child("Issues").child(push).child("latitude").setValue(latitude);
+                dref.child("Issues").child(push).child("driverid").setValue(uid);
+                dref.child("Issues").child(push).child("issue").setValue("I found an item.");
 
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getApplicationContext());
+                alertDialogBuilder.setTitle("Issue Submitted").setMessage("We have recieved your query we will get you soon.").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).show();
 
+            }
+        } );
+
+        tripIssue.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String push = FirebaseDatabase.getInstance().getReference().child("History").push().getKey();
+                dref.child("Issues").child(push).child("id").setValue(push);
+                dref.child("Issues").child(push).child("longitude").setValue(longitude);
+                dref.child("Issues").child(push).child("latitude").setValue(latitude);
+                dref.child("Issues").child(push).child("driverid").setValue(uid);
+                dref.child("Issues").child(push).child("issue").setValue("I have an issue with my trip.");
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getApplicationContext());
+                alertDialogBuilder.setTitle("Issue Submitted").setMessage("We have recieved your query we will get you soon.").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).show();
+
+            }
+        } );
+        appIsuue.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String push = FirebaseDatabase.getInstance().getReference().child("History").push().getKey();
+                dref.child("Issues").child(push).child("id").setValue(push);
+                dref.child("Issues").child(push).child("longitude").setValue(longitude);
+                dref.child("Issues").child(push).child("latitude").setValue(latitude);
+                dref.child("Issues").child(push).child("driverid").setValue(uid);
+                dref.child("Issues").child(push).child("issue").setValue("I have an issue with my app.");
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getApplicationContext());
+                alertDialogBuilder.setTitle("Issue Submitted").setMessage("We have recieved your query we will get you soon.").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).show();
+
+            }
+        } );
 
     }
 
